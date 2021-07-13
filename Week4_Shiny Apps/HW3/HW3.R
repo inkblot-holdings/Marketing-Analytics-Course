65910f61c0a133a2d2ac10e7a0ef1cd42e7a4a0b
kennethrfaro <kennethrfaro@gmail.com>
2021-07-13 10:07:30 -0400

    week4 and 5 updates

---
+++ Week4_Shiny Apps/HW3/HW3.R
@@ -0,0 +1,34 @@
+# Homework: 3
+
+# Date Of Last Update: 7/6/2021
+
+# Author: Ken Faro, Ph.D.
+
+# Copyright: All Documents © Dr. Kenneth R. Faro or © Inkblot Holdings, LLC
+
+# License: Faro and Inkblot Educational License For One Semester Use
+
+# Disclosure: These materials are subject to the terms of the "Notice of Faro and 
+#             Inkblot Educational License For One Semester Use", posted in this repository. 
+
+
+###########################################
+########## Homework In R ##########
+###########################################
+
+# BACKGROUND: For this home work assignment, imagine one of your clients comes to you with their "tracker data".
+# "Tracker Data" is a survey companies distribute to consumers each quarter to as about brand perceptions
+# Your brand is "Brand a" in the data file: 
+# brand.ratings <- read.csv("http://goo.gl/IQl8nc")
+
+
+
+# QUESTION 1: Analyze the data in any way you want
+
+# QUESTION 2: Find 1-3 insights from the data about your client's brand, or their competitors
+
+# QUESTION 3: Visualize these insights on a shiny dashboard
+
+# QUESTION 4: Based on the answers above, what recommendations would you make to your client?
+
+# REMINDER: Please submit your R code supporting your answers to the above and the analysis that led you to your conclusion.
---
+++ Week5_Brand Perception/Lecture Code/Untitled.R
@@ -0,0 +1,19 @@
+# Class: 9
+
+# Date Of Last Update: 7/6/2021
+
+# Author: Ken Faro, Ph.D.
+
+# Copyright: All Documents © Dr. Kenneth R. Faro or © Inkblot Holdings, LLC
+
+# License: Faro and Inkblot Educational License For One Semester Use
+
+# Disclosure: These materials are subject to the terms of the "Notice of Faro and 
+#             Inkblot Educational License For One Semester Use", posted in this repository. 
+
+
+###########################################
+########## Homework In R ##########
+###########################################
+
+# BACKGROUND:
